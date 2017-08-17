dc3732c53b761765eb405e2f213fe84700661a06
SamirHussein <samir-hussen@hotmail.com>
2017-08-17 19:59:57 +0200

    FIRST C SHARP PROGRAM
    THIS IS MY FIRST  C SHARB ORGRAM

---
+++ first program/first program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is my first program in c#
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
+
+        }
+    }
+}
